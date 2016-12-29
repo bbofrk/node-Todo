8138865a6036cdc4b66f782632f915d15956526c
bbofrk <mingliwanglau@gmail.com>
2016-12-30 11:46:47 +1300

    add test for get todo by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const todos = [{
+  _id: new ObjectID(),
   text: 'First Test Todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second Test Todo'
 }];
 
@@ -63,5 +66,31 @@ describe('GET /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
+  });
+  describe('GET /todos/:id', () => {
+    it('should get todo by id', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should get 404 if todo not found', (done) => {
+      var hexId = new ObjectID().toHexString();
+      request(app)
+        .get(`/todos/{hexId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should get 404 for non-object ids', (done) => {
+      request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
+    });
   });
 });

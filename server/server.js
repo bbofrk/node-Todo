29ed4cf4e1eb708dc634c522b0aa9b6502cc1415
bbofrk <mingliwanglau@gmail.com>
2016-12-30 11:00:35 +1300

    add get todos route

--- server/server.js
+++ server/server.js
@@ -8,13 +8,21 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
-
+//post toods
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
   todo.save().then((doc) => {
     res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+//get todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
   }, (err) => {
     res.status(400).send(err);
   });
